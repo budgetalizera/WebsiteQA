43cc2d1b18be340920ac748705a5c00fea617909
unknown <budgetalizer.a@gmail.com>
2025-03-24 03:17:43 +0530

    requirement changed

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install dependencies listed in requirements.txt
-pip install --no-cache-dir -r requirements.txt
-
-# Download the spaCy model manually
-python -m spacy download en_core_web_sm
